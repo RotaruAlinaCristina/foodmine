56a6243858aff5b3d1314c6715ec26af2f4bebdb
RotaruAlinaCristina <rotaru.alina.cristina@gmail.com>
2022-09-24 07:57:21 +0300

    Deploy on Alina

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
+
+import path from "path";
 
 import express from "express";
 import cors from "cors";
-import foodRouter from './routers/food.router';
-import userRouter from './routers/user.router';
-import orderRouter from './routers/order.router';
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import orderRouter from "./routers/order.router";
 
-import { dbConnect } from './configs/database.config';
+import { dbConnect } from "./configs/database.config";
 
 dbConnect();
 
@@ -15,16 +17,23 @@ const app = express();
 app.use(express.json());
 //localhost:4200; - frontend
 //localhost:5000; - backend
-app.use(cors({
-    credentials:true,
-    origin:["http://localhost:4200"]
-}));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:4200"],
+  })
+);
 
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
 
-const port =5000;
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("Website served on http://localhost:" + port);
-})
+  console.log("Website served on http://localhost:" + port);
+});
